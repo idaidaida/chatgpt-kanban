baa221a6eb11f2d55045ccec1b7a2617c801c482
Yuhei Ida <yuuhei0311@gmail.com>
2023-03-19 22:05:01 +0900

    modify resources path

--- kanban-chatgpt4/sw.js
+++ kanban-chatgpt4/sw.js
@@ -2,16 +2,16 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
       caches.open('kanban-cache').then((cache) => {
         return cache.addAll([
-          '/',
-          '/index.html',
-          '/styles.css',
-          '/scripts.js',
-          '/fish_tako.png'
-          // 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css',
-          // 'https://cdn.jsdelivr.net/gh/jquery/jquery/dist/jquery.min.js',
-          // 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js',
-          // 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js',
-          // 'https://cdn.jsdelivr.net/npm/sortablejs@1.14.0/Sortable.min.js'
+          '/kanban-chatgpt4',
+          '/kanban-chatgpt4/index.html',
+          '/kanban-chatgpt4/styles.css',
+          '/kanban-chatgpt4/scripts.js',
+          '/kanban-chatgpt4/fish_tako.png',
+          'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css',
+          'https://cdn.jsdelivr.net/gh/jquery/jquery/dist/jquery.min.js',
+          'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js',
+          'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js',
+          'https://cdn.jsdelivr.net/npm/sortablejs@1.14.0/Sortable.min.js'
         ]);
       })
     );
