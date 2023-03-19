7bbb420ccf01d915ada6ccb4f7f8f0d2d0a4341c
Yuhei Ida <yuuhei0311@gmail.com>
2023-03-19 22:06:57 +0900

    modify resources path

--- kanban-chatgpt4/sw.js
+++ kanban-chatgpt4/sw.js
@@ -2,11 +2,11 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
       caches.open('kanban-cache').then((cache) => {
         return cache.addAll([
-          '/kanban-chatgpt4',
-          '/kanban-chatgpt4/index.html',
-          '/kanban-chatgpt4/styles.css',
-          '/kanban-chatgpt4/scripts.js',
-          '/kanban-chatgpt4/fish_tako.png',
+          '/chatgpt-kanban/kanban-chatgpt4/',
+          '/chatgpt-kanban/kanban-chatgpt4/index.html',
+          '/chatgpt-kanban/kanban-chatgpt4/styles.css',
+          '/chatgpt-kanban/kanban-chatgpt4/scripts.js',
+          '/chatgpt-kanban/kanban-chatgpt4/fish_tako.png',
           'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css',
           'https://cdn.jsdelivr.net/gh/jquery/jquery/dist/jquery.min.js',
           'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js',
