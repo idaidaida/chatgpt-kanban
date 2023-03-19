e5924b8862186b4ba504de8e80f819e5ebb19889
Yuhei Ida <yuuhei0311@gmail.com>
2023-03-19 21:49:12 +0900

    update sw js file

--- kanban-chatgpt4/sw.js
+++ kanban-chatgpt4/sw.js
@@ -3,12 +3,12 @@ self.addEventListener('install', (event) => {
       caches.open('kanban-cache').then((cache) => {
         return cache.addAll([
           '/',
-          'index.html',
-          'styles.css',
-          'scripts.js',
-          'fish_tako.png',
+          '/index.html',
+          '/styles.css',
+          '/scripts.js',
+          '/fish_tako.png',
           'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css',
-          'https://code.jquery.com/jquery-3.6.0.min.js',
+          'https://cdn.jsdelivr.net/gh/jquery/jquery/dist/jquery.min.js',
           'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js',
           'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js',
           'https://cdn.jsdelivr.net/npm/sortablejs@1.14.0/Sortable.min.js'
